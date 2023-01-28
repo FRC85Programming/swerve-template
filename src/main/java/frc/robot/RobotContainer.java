c9eb15a492bec817a2c144a03e6c849c4470ce19
Gibson <115591680+Jibby-34@users.noreply.github.com>
2023-01-28 08:48:09 -0500

    Speed changed by april tag area

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,7 +59,7 @@ public class RobotContainer {
             .onTrue(new ZeroGyroscopeCommand(m_drivetrainSubsystem));
 
     new Trigger(m_controller::getYButton)
-            .whileTrue(new TrackAprilTagCommand(m_drivetrainSubsystem, m_visionTracking));
+            .toggleOnTrue(new TrackAprilTagCommand(m_drivetrainSubsystem, m_visionTracking));
     // a button activates brake wheels command
     new Trigger(m_controller::getAButton)
             .whileTrue(new BrakeWheelsCommand(m_drivetrainSubsystem));
--- src/main/java/frc/robot/commands/TrackAprilTagCommand.java
+++ src/main/java/frc/robot/commands/TrackAprilTagCommand.java
@@ -1,24 +1,24 @@
 package frc.robot.commands;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
 
-public class TrackAprilTagCommand extends CommandBase
-{
+public class TrackAprilTagCommand extends CommandBase {
     double wheelAngle = 0.0;
     boolean wheelReset = true;
     private final DrivetrainSubsystem m_drivetrainSubsystem;
     private final VisionTracking m_visionTracking;
-    public TrackAprilTagCommand(DrivetrainSubsystem driveTrain, VisionTracking visionTracking)
-    {
+
+    public TrackAprilTagCommand(DrivetrainSubsystem driveTrain, VisionTracking visionTracking) {
         m_drivetrainSubsystem = driveTrain;
         m_visionTracking = visionTracking;
     }
 
     @Override
-    public void execute(){   
+    public void execute() {
 
-        if (wheelReset == true){
+        if (wheelReset == true) {
             m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0));
             m_visionTracking.setLED(0);
             wheelReset = false;
@@ -27,16 +27,17 @@ public class TrackAprilTagCommand extends CommandBase
         double tx = m_visionTracking.getX();
         double area = m_visionTracking.getArea();
         double tagID = m_visionTracking.getTag();
-        if (tagID > -1){
-            if (area < 4){
-                m_drivetrainSubsystem.drive(new ChassisSpeeds(-12*area+40, 0.0, -tx));
-            } else if (area > 7){
-                m_drivetrainSubsystem.drive(new ChassisSpeeds(2.5*-area+2.5, 0.0, -tx));
+        if (tagID > -1) {
+            if (area < 4) {
+                m_drivetrainSubsystem.drive(new ChassisSpeeds(-0.8 * area + 5, 0.0, -tx));
+            } else if (area > 7) {
+                m_drivetrainSubsystem.drive(new ChassisSpeeds(-0.8 * area, 0.0, -tx));
             } else {
                 m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, -tx));
             }
         }
     }
+
     @Override
     public void end(boolean interrupted) {
         m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
