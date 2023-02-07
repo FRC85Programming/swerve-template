79e55edcebefbc1b34a4d795e8e2597656b14696
Dylan_W <96798645+ddwalters@users.noreply.github.com>
2023-02-07 16:51:07 -0500

    Added pivot command

--- src/main/java/frc/robot/subsystems/ExtendoSubystem.java
+++ src/main/java/frc/robot/subsystems/ExtendoSubystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -12,11 +13,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ExtendoSubystem extends SubsystemBase {
-    public CANSparkMax extendExtendoMotor = new CANSparkMax(Constants.EXTENDO_EXTEND_MOTOR, MotorType.kBrushless);
-    PIDController pid = new PIDController(0, 0, 0);
+    private CANSparkMax extendExtendoMotor = new CANSparkMax(Constants.EXTENDO_EXTEND_MOTOR, MotorType.kBrushless);
+    private CANSparkMax pivotTelescopeMotor = new CANSparkMax(Constants.EXTENDO_PIVOT_MOTOR, MotorType.kBrushless);
+    private PIDController pid = new PIDController(0, 0, 0);
 
     public void ExtendTelescope(int setPoint){   
         // uses pid controller to get position that is then set to motor
         extendExtendoMotor.set(pid.calculate(extendExtendoMotor.getEncoder().getPosition(), setPoint));
+    }
+
+    /**
+    * @param position takes in a position setter for location of arm 
+    **/ 
+    public void PivotTelescope(double position)
+    {
+        pivotTelescopeMotor.set(pid.calculate(pivotTelescopeMotor.getEncoder().getPosition(), position));
     }
 }
