54adbb2722dec7bb3894e5382ab253e06fa3a517
FRC85DriverStation <Aedanvongsouthi@gmail.com>
2023-02-07 19:35:07 -0500

    Added comments and change the limelight turn on

--- src/main/java/frc/robot/commands/TrackReflective.java
+++ src/main/java/frc/robot/commands/TrackReflective.java
@@ -22,28 +22,31 @@ public class TrackReflective extends CommandBase
     @Override
     public void execute() {
         m_visionTracking.setLED(0);
+        // Sets the limelight LEDs to "Force on"
         double tx = m_visionTracking.getX();
         double area = m_visionTracking.getArea();
+        // Grabs the tapes distance from the crosshair (tx) and the area the tape takes up on the screen (area) as a percentage
         if (area >= 1) {
             m_drivetrainSubsystem.drive(new ChassisSpeeds(3, 0.0, -tx * area * .08));
+            // Makes the robot drive slower when the tape is closer
         }
          else if (area < 1 && area >= .1) {
             m_drivetrainSubsystem.drive(new ChassisSpeeds(3, 0.0, -tx * area * 2));
-        } 
+            // Makes the robot drive a little faster when the tape is medium distance
+         }
         else if (area < .1 && area > 0) {
             m_drivetrainSubsystem.drive(new ChassisSpeeds(3, 0.0, -tx * area * 11));
+            // Makes the robot drive faster when the tape is very far away
         } 
-        else {}
-        
-
-        
-         
+        else {} // Stops the robot if it detects nothing
     }
-
 
     @Override
     public void end(boolean interrupted)  {
         m_visionTracking.setLED(1);
+        // Sets limelight LEDs to "Force off"
         m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+        // Tells the robot to stop moving even if it detects someone
     }
 }
+ 
