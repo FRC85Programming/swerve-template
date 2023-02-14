13a0a457895c88d548efbf36bc7166d10edac070
Dylan_W <96798645+ddwalters@users.noreply.github.com>
2023-02-14 18:53:24 -0500

    re-added roller speed smartdashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    SmartDashboard.putNumber("Upper Roller Speed", -1);
+    SmartDashboard.putNumber("Lower Roller Speed", -1);
     SmartDashboard.putNumber("AutoLevel Constant", 0.2);
     SmartDashboard.putNumber("AutoLevel Max Speed", .2);
     
