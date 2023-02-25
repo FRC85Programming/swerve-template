1acc21742d812ec30236bb50b1b46de23e5d8a24
Ashley Cramer <ashleymcramer@icloud.com>
2023-02-25 14:47:31 -0500

    Lots of Command Stuff

--- src/main/java/frc/robot/commands/CS.java
+++ src/main/java/frc/robot/commands/CS.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.*;
 
@@ -9,8 +10,9 @@ public class CS extends SequentialCommandGroup {
     public CS(DrivetrainSubsystem driveTrain, RobotContainer robotContainer) {
         this.m_robotContainer = robotContainer;
         addCommands(
-            // add wait command
             m_robotContainer.getAutonomousCommand("PW_CS"),
+            new WaitCommand(.5),
+            m_robotContainer.getAutonomousCommand("PW_CSPart2"),
             new AutoLevelCommand(driveTrain)
         );
     }
