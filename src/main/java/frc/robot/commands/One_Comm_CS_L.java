2a9d2b79dc256306e814a83a8f4fed9c4416ab2e
Ashley Cramer <ashleymcramer@icloud.com>
2023-02-27 18:07:20 -0500

    added new autons plus way to call auto

--- src/main/java/frc/robot/commands/One_Comm_CS_L.java
+++ src/main/java/frc/robot/commands/One_Comm_CS_L.java
@@ -1,13 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.*;
 
-public class One_Comm_L extends SequentialCommandGroup {
+public class One_Comm_CS_L extends SequentialCommandGroup {
     private final RobotContainer m_robotContainer;
     private final ExtendoSubystem m_ExtendoSubsystem;
-    public One_Comm_L(DrivetrainSubsystem driveTrain, RobotContainer robotContainer, double extendPosition, double pivotAngle, double intakeWrist, boolean output,
+    public One_Comm_CS_L(DrivetrainSubsystem driveTrain, RobotContainer robotContainer, double extendPosition, double pivotAngle, double intakeWrist, boolean output,
     ExtendoSubystem extendoSubsystem, IntakeSubsystem intakeSubsystem){
       this.m_robotContainer = robotContainer;
       this.m_ExtendoSubsystem = extendoSubsystem;
@@ -15,7 +16,10 @@ public class One_Comm_L extends SequentialCommandGroup {
         new PivotCommand(m_ExtendoSubsystem),
         new ExtendCommand(m_ExtendoSubsystem, intakeSubsystem, extendPosition, pivotAngle, intakeWrist),
         new IntakeCommand(intakeSubsystem, output),
-        m_robotContainer.getAutonomousCommand("PW_1_Comm_L")
+        m_robotContainer.getAutonomousCommand("PW_1_Comm_CS_L"),
+        new WaitCommand(.5),
+        m_robotContainer.getAutonomousCommand("PW_1_Comm_CS_LPart2"),
+        new AutoLevelCommand(driveTrain)
       );
     }
-}  
+} 
