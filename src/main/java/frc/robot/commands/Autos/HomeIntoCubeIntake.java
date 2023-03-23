0abc28e151c3b645665d91c8c8df52110cc6fb2d
Gibson <115591680+Jibby-34@users.noreply.github.com>
2023-03-23 10:30:35 -0400

    Even more speed cuts

--- src/main/java/frc/robot/commands/Autos/HomeIntoCubeIntake.java
+++ src/main/java/frc/robot/commands/Autos/HomeIntoCubeIntake.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Autos;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Arm.ExtendCommand;
 import frc.robot.commands.Arm.HomeExtendCommand;
 import frc.robot.commands.Chassis.DriveDistance;
@@ -9,12 +10,12 @@ import frc.robot.subsystems.ExtendoSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.VisionTracking;
 
-public class StrafeAndHome extends ParallelCommandGroup {
+public class HomeIntoCubeIntake extends SequentialCommandGroup {
 
-    public StrafeAndHome(DrivetrainSubsystem m_DrivetrainSubsystem, VisionTracking vision, ExtendoSubsystem m_ExtendoSubsystem, IntakeSubsystem m_IntakeSubsystem, double target){
+    public HomeIntoCubeIntake(DrivetrainSubsystem m_DrivetrainSubsystem, VisionTracking vision, ExtendoSubsystem m_ExtendoSubsystem, IntakeSubsystem m_IntakeSubsystem, double target){
         addCommands(
-            new StrafeIntoDrive(m_DrivetrainSubsystem, vision, m_ExtendoSubsystem, m_IntakeSubsystem, target),
-            new HomeIntoCubeIntake(m_DrivetrainSubsystem, vision, m_ExtendoSubsystem, m_IntakeSubsystem, target)
+            new HomeExtendCommand(m_ExtendoSubsystem),
+            new AutoIntakeCube(m_ExtendoSubsystem, m_IntakeSubsystem)
         );
     }
 
