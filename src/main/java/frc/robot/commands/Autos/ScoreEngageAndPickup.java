2b3a6b4edd84f78b4c650e87adcf532df10be15c
Gibson <115591680+Jibby-34@users.noreply.github.com>
2023-03-23 20:33:24 -0400

    Commit numero dos

--- src/main/java/frc/robot/commands/Autos/ScoreEngageAndPickup.java
+++ src/main/java/frc/robot/commands/Autos/ScoreEngageAndPickup.java
@@ -24,8 +24,8 @@ public class ScoreEngageAndPickup extends SequentialCommandGroup {
             //new DriveAndIntake(driveTrain, vision, extendo, intake, 1.2),
             new DriveAndHomeCommand(driveTrain, vision, extendo, intake, 4.7),
             new AutoLevelPIDCommand(driveTrain)*/
-            new DriveDistance(driveTrain, vision, 0, -3, 0, 15),
-            new DriveDistance(driveTrain, vision, 0, 0, 5, 0)
+            new DriveDistance(driveTrain, vision, 0, -3, 0, 15, 0, false),
+            new DriveDistance(driveTrain, vision, 0, 0, 5, 0, 0, false)
         );
     }
 }
