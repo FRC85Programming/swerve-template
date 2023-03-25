3d021c20135dd2f40a7cd8fa92266c8f15cf1cfb
Gibson DeGroot <115591680+Jibby-34@users.noreply.github.com>
2023-03-25 00:12:39 -0400

    Changed entire vision command, now complete with speed control and a P of PID

--- src/main/java/frc/robot/commands/Autos/ScoreLineup.java
+++ src/main/java/frc/robot/commands/Autos/ScoreLineup.java
@@ -8,57 +8,75 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ScoreLineup extends CommandBase
 {
-
-    private final DrivetrainSubsystem m_drivetrainSubsystem;
-    public Boolean driveDone;
-    public Boolean track;
-    public Boolean trackDone;
-    private final VisionTracking vision;
-    Boolean timerStarted = false;
-    private double areaValue;
-    private Boolean areaChecked = false;
-    private double tx;
-    private double area;
-    boolean driveLeftStarted = false;
-    boolean driveRightStarted = false;
-    private double turnMultiply = 1;
     public ScoreLineup(DrivetrainSubsystem driveTrain, VisionTracking vision) {
+        createSystems();
+        init();
         m_drivetrainSubsystem = driveTrain;
         this.vision = vision;
-        trackDone = false;
         addRequirements(m_drivetrainSubsystem);
     }
 
     @Override
     public void execute() {
-        vision.setPipeline(2);
+        switchPipeline();
+        collectValues();
+        setCorrectionValues();
+        
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, steering_adjust));
+        
+    }
+
+    public void init() {
+        // Variables are created and set to default values
+        private double tx;
+        private double area;
+        // PID values that adjust the distance from the offset
+        double headingError;
+        double Kp = 0.1f;
+        double steering_adjust;
+        // Switch boolean that makes sure we only set the pipline once
+        double lineSwitched = false;
+        vision.setLED(0);
+    }
+    
+    public void createSystems() {
+        // Our two subsystems for this are set up. Vision for detecting cubes and Drivetrain to correct our rotation
+        private final DrivetrainSubsystem m_drivetrainSubsystem;
+        private final VisionTracking vision;
+    }
+
+    public void collectValues() {
+        // Sets the values of the targets position ad size
         tx = vision.getX();
         area = vision.getArea();
+    }
 
-        if (areaChecked == false) {
-            areaValue = area;
-            areaChecked = true;
-        }
-        if (tx-0.2 > -3.441302*areaValue-3.473436 && tx +0.2 < -3.441302*areaValue-3.473436) {
-            turnMultiply = 0;
-        }
-        if (tx < -3.441302*areaValue-3.473436) {
-            m_drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, turnMultiply*0.5));
-        } else if (tx < -3.441302*areaValue-3.473436) {
-            m_drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, turnMultiply*-0.5));
+    public void setCorrectionValues() {
+        // Sets the values that will be driven in proportion to feedback
+        headingError = tx;
+        steering_adjust = Kp * tx;
+    }
+
+    public void switchPipeline() {
+        // This if statement makes sure we only switch the pipline once so our camera doesn't lag
+        if (lineSwitched == false) {
+            vision.setPipeline(2);
+            lineSwitched = true;
         }
     }
 
 
     @Override
-    public boolean isFinished(){
-       return turnMultiply == 0 || tx-0.2 > -3.441302*areaValue-3.473436 && tx +0.2 < -3.441302*areaValue-3.473436;
+    public boolean isFinished(){ 
+        // Latency window of x to be lined up
+       return headingError-0.2 < 0 && headingError+0.2 > 0;
     }
 
     @Override
     public void end (boolean interrupted)  {
         // Stops the robot and allows the target distance to be calculated again
         m_drivetrainSubsystem.drive(new ChassisSpeeds(0,0, 0));
-        areaChecked = false;
+        lineSwitched = false;
+        vision.setLED(1);
     }
 }
